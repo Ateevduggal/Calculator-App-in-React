8719e5ed359a385d9b369ad515f027a7e8535b85
AteevDuggal <akd3257@gmail.com>
2022-01-07 09:02:15 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useState } from "react";
+import Data from "./Data";
+const App = () => {
+  const [result, setResult] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container-fluid">
+        <div className="row" id="main">
+        <dic class="col-md-12 col-sm-12 my-5 text-center fs-1 mb-3 fw-bolder">Calculator</dic>
+          <div className="col-md-4 col-sm-3"></div>
+          <div className="col-md-4 col-sm-6 p-3 bg-dark">
+            <div id="screen" className="col-12 bg-dark text-light text-end p-3">
+              <input
+                placeholder="0"
+                value={result}
+                className="fs-1 fw-bold mx-3 col-12 bg-dark text-light border-0 text-end bg"
+              />
+            </div>
+            <input
+              type="button"
+              value="Del"
+              className="btn btn-secondary text-dark fw-bold fs-5 px-3 ms-1 mt-2"
+              onClick={(e) => setResult(result.slice(0, -1))}
+            />
+            <input
+              type="button"
+              value="Clear"
+              className="btn btn-secondary text-dark fw-bold fs-5 px-3 ms-1 mt-2"
+              onClick={() => setResult("")}
+            />
+            {Data.map((Val) => {
+              const { symbol, className } = Val;
+              return (
+                <>
+                  <input
+                    type="button"
+                    value={symbol}
+                    className={`${className}`}
+                    onClick={(e) => setResult(result.concat(e.target.value))}
+                  />
+                </>
+              );
+            })}
+            <input
+              type="button"
+              value="="
+              className="btn btn-warning text-dark fs-5 px-3 ms-1 mt-2"
+              onClick={() => setResult(eval(result).toString())}
+            />
+          </div>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/Data.js
@@ -0,0 +1,71 @@
+const Data = [
+  {
+    symbol: "/",
+    className: "btn btn-secondary text-dark fw-bold fs-5 px-2 ms-1 mt-2",
+  },
+  {
+    symbol: "%",
+    className: "btn btn-warning text-dark fs-5 ms-1 mt-2",
+  },
+  {
+    symbol: "7",
+    className: "btn btn-dark text-light fs-5 px-2 ms-1 mt-2",
+  },
+  {
+    symbol: "8",
+    className: "btn btn-dark text-light fs-5 px-2 ms-1 mt-2",
+  },
+  {
+    symbol: "9",
+    className: "btn btn-dark text-light fs-5 px-2 ms-1 mt-2",
+  },
+  {
+    symbol: "*",
+    className: "btn btn-warning text-dark fs-5 p2-3 ms-1 mt-2",
+  },
+  {
+    symbol: "4",
+    className: "btn btn-dark text-light fs-5 px-2 ms-1 mt-2",
+  },
+  {
+    symbol: "5",
+    className: "btn btn-dark text-light fs-5 px-2 ms-1 mt-2",
+  },
+  {
+    symbol: "6",
+    className: "btn btn-dark text-light fs-5 px-2 ms-1 mt-2",
+  },
+  {
+    symbol: "-",
+    className: "btn btn-warning text-dark fs-5 p2-3 ms-1 mt-2",
+  },
+  {
+    symbol: "1",
+    className: "btn btn-dark text-light fs-5 px-2 ms-1 mt-2",
+  },
+  {
+    symbol: "2",
+    className: "btn btn-dark text-light fs-5 px-2 ms-1 mt-2",
+  },
+  {
+    symbol: "3",
+    className: "btn btn-dark text-light fs-5 px-2 ms-1 mt-2",
+  },
+  {
+    symbol: "+",
+    className: "btn btn-warning text-dark fs-5 p2-3 ms-1 mt-2",
+  },
+  {
+    symbol: "+/-",
+    className: "btn btn-dark text-light fs-5 px-2 ms-1 mt-2",
+  },
+  {
+    symbol: "0",
+    className: "btn btn-dark text-light fs-5 px-2 ms-1 mt-2",
+  },
+  {
+    symbol: ".",
+    className: "btn btn-dark text-light fs-5 px-2 ms-1 mt-2",
+  },
+];
+export default Data;
